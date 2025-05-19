56c5bab79fabfbfd79a53cfc2386ff37b91c663f
Imaraan Sheldon <imaraansheldonis2302003@gmail.com>
2025-05-19 13:42:53 +0200

    progress made

--- scripts/script.js
+++ scripts/script.js
@@ -50,3 +50,12 @@ let cursorX = 0,
   };
 
   requestAnimationFrame(borderAnimation);
+
+
+  function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
